cd1742ec74981b8ed88ce8debf770197daefa734
Logan Brock <logan.g.brock@gmail.com>
2021-06-30 12:08:31 -0400

    Cannot render posts in Posts.jsx - map is not a function - need to make into an array

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,15 +1,19 @@
-import "./header.css"
+import './header.css';
 
-export const Header = () => {
-    return (
-        <div className="header">
-            <div className="headerTitles">
-                <span className="headerTitleSm">React & Node</span>
-                <span className="headerTitleLg">Blog</span>
-            </div>
-            <img className="headerImg" src="https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d" alt="headerImg" />
-        </div>
-    )
-}
+const Header = () => {
+	return (
+		<div className='header'>
+			<div className='headerTitles'>
+				<span className='headerTitleSm'>Dog Name</span>
+				<span className='headerTitleLg'>Blog</span>
+			</div>
+			<img
+				className='headerImg'
+				src='https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d'
+				alt='headerImg'
+			/>
+		</div>
+	);
+};
 
-export default Header
+export default Header;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,18 +1,29 @@
-import Header from "../../components/header/Header"
-import Posts from "../../components/posts/Posts"
-import Sidebar from "../../components/sidebar/Sidebar"
-import "./home.css"
+import { useEffect, useState } from 'react';
+import Header from '../../components/header/Header';
+import Posts from '../../components/posts/Posts';
+import Sidebar from '../../components/sidebar/Sidebar';
+import './home.css';
+import axios from 'axios';
 
-export const Home = () => {
-    return (
-        <>
-        <Header />
-        <div className="home">
-            <Posts />
-            <Sidebar />
-        </div>
-        </>
-    )
-}
+const Home = () => {
+	const [posts, setPosts] = useState([]);
 
-export default Home
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const res = await axios.get('/');
+			setPosts(res.data);
+		};
+		fetchPosts();
+	}, []);
+	return (
+		<>
+			<Header />
+			<div className='home'>
+				<Posts posts={posts} />
+				<Sidebar />
+			</div>
+		</>
+	);
+};
+
+export default Home;
