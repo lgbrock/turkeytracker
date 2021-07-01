34005c0b313d3e1fd4ddc5e0250e3fd006b4ed69
Logan Brock <logan.g.brock@gmail.com>
2021-06-30 21:50:35 -0400

    finshed saving current user to local storage with Context API

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,22 +1,57 @@
+import axios from 'axios';
+import { useContext, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import "./login.css";
+import { Context } from '../../context/Context';
+import './login.css';
 
 const Login = () => {
-    return (
-        <div className="login">
-            <span className="loginTitle">Login</span>
-            <form className="loginForm">
-                <label>Email</label>
-                <input className="loginInput" type="text" placeholder="Enter your email..." />
-                <label>Password</label>
-                <input className="loginInput" type="password" placeholder="Enter your password..." />
-                <button className="loginButton">Login</button>
-                <button className="loginRegisterButton">
-                    <Link className="link" to="/register">Register</Link>
-                </button>
-            </form>
-        </div>
-    )
-}
+	const userRef = useRef();
+	const passwordRef = useRef();
+	const { dispatch, isFetching } = useContext(Context);
 
-export default Login
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		dispatch({ type: 'LOGIN_START' });
+		try {
+			const res = await axios.post('/auth/login', {
+				username: userRef.current.value,
+				password: passwordRef.current.value,
+			});
+			dispatch({ type: 'LOGIN_SUCCESS', payload: res.data });
+		} catch (err) {
+			dispatch({ type: 'LOGIN_FAILURE' });
+		}
+	};
+	console.log(isFetching);
+	return (
+		<div className='login'>
+			<span className='loginTitle'>Login</span>
+			<form className='loginForm' onSubmit={handleSubmit}>
+				<label>Username</label>
+				<input
+					className='loginInput'
+					type='text'
+					placeholder='Enter your username...'
+					ref={userRef}
+				/>
+				<label>Password</label>
+				<input
+					className='loginInput'
+					type='password'
+					placeholder='Enter your password...'
+					ref={passwordRef}
+				/>
+				<button className='loginButton' type='submit' disabled={isFetching}>
+					Login
+				</button>
+				<button className='loginRegisterButton' type='submit'>
+					<Link className='link' to='/register'>
+						Register
+					</Link>
+				</button>
+			</form>
+		</div>
+	);
+};
+
+export default Login;
