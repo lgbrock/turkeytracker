17eede2b6fe313d50c3da03b868683dead632125
Logan Brock <logan.g.brock@gmail.com>
2021-06-30 22:02:12 -0400

    added profile picture and logout tab to Navbar

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -1,63 +1,66 @@
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { Context } from '../../context/Context';
 import './navbar.css';
 
 const NavBar = () => {
-	const user = false;
+	const { user } = useContext(Context);
+
 	return (
-		<div className="nav">
-			<div className="navLeft">
-				<i className="navIcon fab fa-github"></i>
-				<i className="navIcon fab fa-twitter"></i>
-				<i className="navIcon fab fa-facebook"></i>
-				<i className="navIcon fab fa-instagram"></i>
+		<div className='nav'>
+			<div className='navLeft'>
+				<i className='navIcon fab fa-github'></i>
+				<i className='navIcon fab fa-twitter'></i>
+				<i className='navIcon fab fa-facebook'></i>
+				<i className='navIcon fab fa-instagram'></i>
 			</div>
-			<div className="navCenter">
-				<ul className="navList">
-					<li className="navListItem">
-						<Link className="link" to="/">
+			<div className='navCenter'>
+				<ul className='navList'>
+					<li className='navListItem'>
+						<Link className='link' to='/'>
 							Home
 						</Link>
 					</li>
-					<li className="navListItem">
-						<Link className="link" to="/">
+					<li className='navListItem'>
+						<Link className='link' to='/'>
 							About
 						</Link>
 					</li>
-					<li className="navListItem">
-						<Link className="link" to="/">
+					<li className='navListItem'>
+						<Link className='link' to='/'>
 							Contact
 						</Link>
 					</li>
-					<li className="navListItem">
-						<Link className="link" to="/write">
+					<li className='navListItem'>
+						<Link className='link' to='/write'>
 							Write
 						</Link>
 					</li>
-					<li className="navListItem">{user && 'Logout'}</li>
+					<li className='navListItem'>{user && 'Logout'}</li>
 				</ul>
 			</div>
-			<div className="navRight">
+			<div className='navRight'>
 				{user ? (
 					<img
-						className="navImg"
-						src="https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
-						alt="profilePicture"
+						className='navImg'
+						src='https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg'
+						alt='profilePicture'
 					/>
 				) : (
-					<ul className="navList">
-						<li className="navListItem">
-							<Link className="link" to="/login">
+					<ul className='navList'>
+						<li className='navListItem'>
+							<Link className='link' to='/login'>
 								Login
 							</Link>
 						</li>
-						<li className="navListItem">
-							<Link className="link" to="/register">
+						<li className='navListItem'>
+							<Link className='link' to='/register'>
 								Register
 							</Link>
 						</li>
 					</ul>
 				)}
-				<i className="navBarIcon fas fa-search"></i>
+				<i className='navBarIcon fas fa-search'></i>
 			</div>
 		</div>
 	);
