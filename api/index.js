d0724de099f1d24ee5d953f966df4d44ac7a8ede
Logan Brock <logan.g.brock@gmail.com>
2021-07-07 19:56:36 -0400

    trying to get Heroku app to work

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ const postRoute = require('./routes/posts');
 const categoryRoute = require('./routes/categories');
 const multer = require('multer');
 const path = require('path');
+const PORT = process.env.PORT || 5000
 
 dotenv.config();
 app.use(express.json());
@@ -42,6 +43,6 @@ app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
 app.use('/api/categories', categoryRoute);
 
-app.listen('5000', () => {
-	console.log('Backend is running...');
+app.listen(PORT, () => {
+	console.log(`Backend is running on ${PORT}...`);
 });
